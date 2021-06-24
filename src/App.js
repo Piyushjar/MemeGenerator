3dd81761a686e97ca0cfd232ee11d09225f91cb1
Piyushraj <rajpiyush1002@gmail.com>
2021-06-24 18:06:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import Header from "./MyComponents/header";
+import MemeGenerator from "./MyComponents/MemeGenerator";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+export default App;
---
+++ src/MyComponents/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1>
+      <header>
+        <img
+          src="http://www.pngall.com/wp-content/uploads/2018/05/Epic-Face-High-Quality-PNG.png"
+          alt="Problem?"
+        />
+        <p>Meme Maker</p>
+      </header>
+    </h1>
+  );
+}
+
+export default Header;
